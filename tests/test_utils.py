110052aeb39273e7a4a590d0d92ea8c460f3590b
Anthony <azelinsky1996@gmail.com>
2021-02-22 20:48:26 -0500

    Removing non tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -59,17 +59,6 @@ def test_load_json():
     with open(os.path.join("tests", "test_files", "testing_json.json"), "r") as infile:
         assert result == load_json(infile)
 
-# @mock_s3
-def my_model_save():
-    conn = boto3.resource('s3', region_name='us-east-1')
-    # We need to create the bucket since this is all in Moto's 'virtual' AWS account
-    conn.create_bucket(Bucket='mybucket')
-
-    model_instance = MyModel('steve', 'is awesome')
-    model_instance.save()
-
-    body = conn.Object('mybucket', 'steve').get()['Body'].read().decode("utf-8")
-
 
 @moto.mock_s3
 def test_read_file_from_bucket():
