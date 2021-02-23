4666df7eebf38c585b3459db48f291e48b545b74
Anthony <azelinsky1996@gmail.com>
2021-02-22 20:47:51 -0500

    filling out more unit tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,22 @@
-import pytest
 import os
+import boto3
+import moto
+import pytest
 
 from parks_and_ride.lambda_functions.utils.lamb_utilities import (
     determine_file_type, retrieve_values, retrieve_record, validate_attributes,
-    load_json
+    load_json, read_file_from_bucket
 )
+
+
+class MyModel(object):
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+    def save(self):
+        s3 = boto3.client('s3', region_name='us-east-1')
+        s3.put_object(Bucket='test_bucket', Key=self.name, Body=self.value)
 
 
 def test_determine_file_type():
@@ -46,3 +58,27 @@ def test_load_json():
     }
     with open(os.path.join("tests", "test_files", "testing_json.json"), "r") as infile:
         assert result == load_json(infile)
+
+# @mock_s3
+def my_model_save():
+    conn = boto3.resource('s3', region_name='us-east-1')
+    # We need to create the bucket since this is all in Moto's 'virtual' AWS account
+    conn.create_bucket(Bucket='mybucket')
+
+    model_instance = MyModel('steve', 'is awesome')
+    model_instance.save()
+
+    body = conn.Object('mybucket', 'steve').get()['Body'].read().decode("utf-8")
+
+
+@moto.mock_s3
+def test_read_file_from_bucket():
+    conn = boto3.resource('s3', region_name='us-east-1')
+    conn.create_bucket(Bucket="test_bucket")
+
+    model_instance = MyModel("coding", "is_awesome")
+    model_instance.save()
+    assert "is_awesome" == read_file_from_bucket(bucket_name="test_bucket", file_name="coding").read().decode("utf-8")
+    
+
+
