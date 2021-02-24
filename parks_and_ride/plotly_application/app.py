594faff2f27e7e8c7bf829425debd339767de313
Anthony <azelinsky1996@gmail.com>
2021-02-24 13:09:50 -0500

    Building rough figure of graph. Added colored points against backdrop of map. Need to add textual data and come up with a method to contnuously update and reload  the map each time new data is added.

--- parks_and_ride/plotly_application/app.py
+++ parks_and_ride/plotly_application/app.py
@@ -1,6 +1,9 @@
 import os
 import sys
-import plotly.express as px
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objects as go
 sys.path.append(os.path.join(os.getcwd(), "parks_and_ride", "lambda_functions"))
 
 from data_access import access_data
@@ -9,5 +12,59 @@ from data_access import access_data
 MAPBOX_ACCESS_TOKEN = open(os.path.join(os.getcwd(), "mapbox_access_token")).read()
 
 
+def set_color(x):
 
-print(access_data())
+    if(x < 100):
+        return "red"
+    elif(x >= 100 | x <= 400):
+        return "yellow"
+    elif(x > 400):
+        return "green"
+    else:
+        return "blue"
+
+
+def update_figure(fig):
+    model_data = access_data()
+    fig.add_trace(go.Scattermapbox(
+        lat=model_data.latitude,
+        lon=model_data.longitude,
+        mode='markers',
+        marker=go.scattermapbox.Marker(
+            size=8,
+            color= list(map(set_color, map(lambda x: int(x), model_data.spaces)))
+        ),
+        text=model_data.title,
+        hoverinfo='text'
+    ))
+
+    return fig, model_data
+
+
+def create_figure():
+    fig = go.Figure()
+
+
+    fig, model_data = update_figure(fig)
+    
+    fig.update_layout(
+        title='Parks and Ride Lot Locations',
+        autosize=True,
+        hovermode='closest',
+        showlegend=False,
+        mapbox=dict(
+            accesstoken=MAPBOX_ACCESS_TOKEN,
+            bearing=0,
+            center=dict(
+                lat=model_data.latitude.mean(),
+                lon=model_data.longitude.mean()
+            ),
+            pitch=0,
+            zoom=5,
+            style='light'
+        ),  
+    )
+    fig.show()
+
+
+create_figure()
--- parks_and_ride/plotly_application/data_access.py
+++ parks_and_ride/plotly_application/data_access.py
@@ -1,9 +1,16 @@
+import pandas as pd
+
 from data_modelling.database_interaction import (
     LotInformation, DatabaseAccessor, DatabaseConnection
 )
 from data_modelling.connection_details import ConnectionDetails
 
 
+def format_models_for_graph_use(models):
+    return pd.DataFrame(models)
+    
+
 def access_data():
     database_accessor = DatabaseAccessor(ConnectionDetails, DatabaseConnection)
-    return database_accessor.load_data_models(LotInformation)
+    models = database_accessor.load_data_models(LotInformation)
+    return format_models_for_graph_use(models)
